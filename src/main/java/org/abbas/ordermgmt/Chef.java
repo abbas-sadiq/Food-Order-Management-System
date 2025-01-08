3a5d61a6b04418ebb1bb755526c3239cc710334b
Abbas <abbeekhan786@gmail.com>
2025-01-08 19:54:31 +0500

    update chef

--- src/main/java/org/abbas/ordermgmt/Chef.java
+++ src/main/java/org/abbas/ordermgmt/Chef.java
@@ -2,6 +2,16 @@ package org.abbas.ordermgmt;
 
 public class Chef {
     private Counter counter;
+    OrderQueue orderQueue = new OrderQueue();
+
+    public void gitOrderFromCounter(Order order){
+        System.out.printf("Chef: preparing order for table : %s ", order.getTableNo());
+        orderQueue.enQueue(counter.pickToPrepare());
+    }
+    public Order deliverOrderToCounter(){
+        System.out.println("Chef: order is prepared.");
+        return orderQueue.deQueue();
+    }
 
 
 }
