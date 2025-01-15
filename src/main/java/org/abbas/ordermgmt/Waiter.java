4f985c845a4ff573df103b5958a24ca33df49ab8
Abbas <abbeekhan786@gmail.com>
2025-01-15 13:47:48 +0500

    some changing in waiter

--- src/main/java/org/abbas/ordermgmt/Waiter.java
+++ src/main/java/org/abbas/ordermgmt/Waiter.java
@@ -4,48 +4,48 @@ package org.abbas.ordermgmt;
 
 
 public class Waiter {
-    private Counter counter;
+
     OrderQueue Waiter_orderQueue = new OrderQueue();
+    private Menu menu;
 
-    public Waiter(Counter counter) {
-        this.counter = counter;
+
+    public Waiter(Menu menu) {
+        this.menu = menu;
     }
 
-    public void takeOrder(Order order){
-        System.out.printf("Waiter: Taking order:: tableNo: %S, pizza: %s, burger: %s, Fries: %s, cold drink: %s",
-                order.getTableNo(), order.getPizza(),
-                order.getBurger(), order.getFries(), order.getColdDrinks());
+    public void takeOrder(Order order, int tableNo){
+        order.setTableNo(tableNo);
         Waiter_orderQueue.enQueue(order);
-
     }
 
 
-    public void placeNewOrderToCounter(){
+    public void placeNewOrderToCounter(Counter counter){
 
         if(Waiter_orderQueue.isEmpty()){
             System.out.println("No order in queue");
             return;
         }
         Order order = Waiter_orderQueue.deQueue();
-        System.out.printf("Preparing order for table %s: Pizza: %s, Burger: %s, Fries: %s, Cold Drink: %s\n",
-                order.getTableNo(), order.getPizza(), order.getBurger(), order.getFries(), order.getColdDrinks());
         counter.submitToPrepare(order);
     }
 
-    public void deliverOrderToTable() {
+    public void deliverOrderToTable(Counter counter) {
         Order order = counter.pickToDeliver();
         if (order == null) {
             System.out.println("No orders to deliver!");
             return;
         }
-        System.out.printf("Order Delivered :: Pizza: %s, Burger: %s, Fries: %s, Cold Drink: %s%n",
-                order.getPizza(), order.getBurger(), order.getFries(), order.getColdDrinks());
+        System.out.println("Waiter: Order Delivered Successfully...");
     }
 
 
     public void checkOrderList() {
         System.out.println("\n" + "==={==============Waiter isChecking orders list==============  \n");
         Waiter_orderQueue.displayOrders();
+    }
+
+    public void displayMenu(){
+        menu.showMenu();
     }
 }
 
