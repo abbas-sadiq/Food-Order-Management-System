8b31278e493808055d764f4219b45ab38104905c
Abbas <abbeekhan786@gmail.com>
2025-01-27 16:50:52 +0500

    initial Implementation

--- src/main/java/org/abbas/ordermgmt/Table.java
+++ src/main/java/org/abbas/ordermgmt/Table.java
@@ -1,4 +1,42 @@
 package org.abbas.ordermgmt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
+     private int no;
+     private int capacity;
+     private List<Table> tables;
+
+    public Table(int no, int capacity) {
+        this.no = no;
+        this.capacity = capacity;
+        this.tables = new ArrayList<>();
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Table> getTables() {
+        return tables;
+    }
+
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
+    }
+
+
 }
