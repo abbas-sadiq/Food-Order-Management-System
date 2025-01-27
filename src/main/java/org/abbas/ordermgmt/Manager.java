116563933b395572cf7a4ad479bfeb1b4bfb866b
Abbas <abbeekhan786@gmail.com>
2025-01-27 16:51:15 +0500

    initial Implementation

--- src/main/java/org/abbas/ordermgmt/Manager.java
+++ src/main/java/org/abbas/ordermgmt/Manager.java
@@ -1,4 +1,16 @@
 package org.abbas.ordermgmt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Manager {
+    private List<Table>tableList;
+
+    public Manager() {
+        this.tableList = new ArrayList<>();
+    }
+
+
+
+
 }
