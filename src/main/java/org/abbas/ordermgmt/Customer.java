431ba5ef3d450da4937d58dacb0add7e098cd881
Abbas <abbeekhan786@gmail.com>
2025-02-04 14:23:00 +0500

    updated Code

--- src/main/java/org/abbas/ordermgmt/Customer.java
+++ src/main/java/org/abbas/ordermgmt/Customer.java
@@ -30,7 +30,6 @@ public class Customer {
         return order;
     }
     public void payBill(Bill bill) {
-        Order order = new Order();
         System.out.println("Customer: Received the bill. Paying now...");
         bill.payBill();
     }
